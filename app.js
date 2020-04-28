fb8442c73f731fbc2769dcfb2da92d55cbfb47cd
yuvaraj-1991 <yuviatudemy2@gmail.com>
2020-04-28 21:09:50 +0530

    added a few changes to project

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 //Listen for Submit
-document.getElementById('loan-form').addEventListener('submit',calculateResults)
+document.getElementById('loan-form').addEventListener('submit',function(e){
+    //Hide Results
+    document.getElementById('results').style.display = 'none'
+    //Show Loader
+    document.getElementById('loading').style.display = 'block'
+
+    setTimeout(calculateResults,2000)
+
+    e.preventDefault()
+})
 
 //Calculate Results
 function calculateResults(e){
@@ -25,15 +34,27 @@ function calculateResults(e){
         monthlyPayment.value = monthly.toFixed(2)
         totalPayment.value = (monthly * calculatedPayments).toFixed(2)
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
+        //Show Results
+        document.getElementById('results').style.display = 'block'
+
+        //Hide Loader
+        document.getElementById('loading').style.display = 'none'
+
     }else {
         showError('Please check your numbers')
     }
 
-    e.preventDefault()
+    
 }
 
 //Show Error
 function showError(error){
+    //Hide Results
+    document.getElementById('results').style.display = 'none'
+
+    //Hide Loader
+    document.getElementById('loading').style.display = 'none'
+
     //Create a div
     const errorDiv = document.createElement('div')
 
